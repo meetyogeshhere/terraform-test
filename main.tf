c54df7a3a5ba32d8e4c3a0eab7596eb1bb9a16df
meetyogeshhere <meetyogeshhere@gmail.com>
2025-10-04 18:09:39 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_treehouse" {
-  ami           = "ami-0c55b159cbfafe1f0" # This is an Amazon Linux image (ask an adult to find the right AMI ID for your region)
+  ami           = "ami-052064a798f08f0d3" # This is an Amazon Linux image (ask an adult to find the right AMI ID for your region)
   instance_type = "t2.micro"              # A small, free-tier computer
   tags = {
     Name = "MyCoolTreehouse"
